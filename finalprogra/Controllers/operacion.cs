ada24cba692eb4310f0d02eedc0341ad2b89df81
Maria Fernanda Ferreira Alfaro <a2018114425@estudiantes.upsa.edu.bo>
2020-12-12 10:55:16 -0400

    ya se realizo mi controlador operaciones

---
+++ finalprogra/Controllers/operacion.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finalprogra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class operacion : ControllerBase
+    {
+        [HttpGet]
+        public string funcion(int numero)
+        {
+            string texto = "";
+            if (numero < 0)
+            {
+                texto = "ERROR";
+            }
+            if (numero == 0)
+            {
+                texto = "Realizado por Fernanda Ferreira";
+            }
+            if (numero > 0)
+            {
+                texto = "imagen: https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg"; 
+            }
+            return texto; 
+        }
+    }
+}
